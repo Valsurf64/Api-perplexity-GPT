3cee896d35b9e4dc37204378c0aea0aecc164eb9
Valsurf64 <valentin.ciancilla@hotmail.com>
2025-04-27 20:32:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def search():
     }
 
     payload = {
-        "model": "pplx-7b-online",
+        "model": "pplx-7b-chat",  # 
         "messages": [
             {"role": "user", "content": f"Donne-moi {nombre_de_resultats} tendances sur : {mot_cle}"}
         ]
